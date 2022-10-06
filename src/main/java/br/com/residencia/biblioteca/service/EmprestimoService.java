7bdcedd4f83d57c7cb7bf9c55db8ae26e378c638
Rafaelfesaro <rafaelfesaro@hotmail.com>
2022-10-05 21:55:13 -0300

    dia 05 de outubro

---
+++ src/main/java/br/com/residencia/biblioteca/service/EmprestimoService.java
@@ -0,0 +1,48 @@
+package br.com.residencia.biblioteca.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.residencia.biblioteca.entity.Emprestimo;
+import br.com.residencia.biblioteca.repository.EmprestimoRepository;
+
+@Service
+public class EmprestimoService {
+	
+	@Autowired
+	EmprestimoRepository emprestimoRepository;
+	
+	public List<Emprestimo> getAllLivro(){
+		return emprestimoRepository.findAll();
+	}
+	
+	public Emprestimo getEmprestimoById(Integer id) {
+		return emprestimoRepository.findById(id).get();
+	}
+	
+	public Emprestimo saveEmprestimo(Emprestimo emprestimo) {
+		return emprestimoRepository.save(emprestimo);
+	}
+	
+	public Emprestimo updateEmprestimo(Emprestimo emprestimo, Integer id) {
+		
+		Emprestimo emprestimoExistenteNoBanco = getEmprestimoById(id);
+		
+//		emprestimoExistenteNoBanco.setCodigoEmprestimo(emprestimo.getCodigoEmprestimo());
+//		emprestimoExistenteNoBanco.setNumeroMatriculaAluno(emprestimo.getNumeroMatriculaAluno());
+//		emprestimoExistenteNoBanco.setCodigoLivro(emprestimo.getCodigoLivro());
+		emprestimoExistenteNoBanco.setDataEmprestimo(emprestimo.getDataEmprestimo());
+		emprestimoExistenteNoBanco.setDataEntrega(emprestimo.getDataEntrega());
+		emprestimoExistenteNoBanco.setValorEmprestimo(emprestimo.getValorEmprestimo());
+		
+		return emprestimoRepository.save(emprestimoExistenteNoBanco);
+	}
+	
+	public Emprestimo deleteEmprestimo(Integer id) {
+		emprestimoRepository.deleteById(id);
+		return getEmprestimoById(id);
+	}
+
+}
